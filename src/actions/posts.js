aa5b72615c69b83fff79ebfc0d02007b06a015cb
Rea Newell <patrick.r.newell@gmail.com>
2018-09-30 16:32:16 -0500

    converted promise chains in post actions to async/await functions

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -14,62 +14,58 @@ export const addPost = ({id, title, body, postDate, categories}) => ({
         categories
     }
 });
-export const startAddPost = (postData = {}, image) => {
-    return (dispatch) => {
-        let currentPost;
-        const {
-            title = "",
-            body = "",
-            postDate = {
-                day: new Date().getDate(),
-                month: new Date().getMonth(),
-                year: new Date().getFullYear()
-            },
-            categories = []
-        } = postData;
-        const post = { title, body, postDate, categories };
+export const startAddPost = (postData = {}, image) => async (dispatch) => {
+    let currentPost;
+    const {
+        title = "",
+        body = "",
+        postDate = {
+            day: new Date().getDate(),
+            month: new Date().getMonth(),
+            year: new Date().getFullYear()
+        },
+        categories = []
+    } = postData;
+    const post = { title, body, postDate, categories };
 
-        // Reference the posts array in database and push post to the end of the array.
-        // If an image paramater exists, store the image and set the link and image name properties on the related post object.
-        return database.ref(`posts`).push(post).then((ref) => {
-            dispatch(addPost({
-                id: ref.key,
-                ...post
-            }));
-            currentPost = ref.key;
-        }).then(() => {
-            if (image) {
-                const fileName = image.name;
-                const storageRef = storage.ref(`blogPictures/${fileName}`);
-                const uploadTask = storageRef.put(image);
-                let downloadURL;
+    // Reference the posts array in database and push post to the end of the array.
+    // If an image paramater exists, store the image and set the link and image name properties on the related post object.
+    const ref = await database.ref(`posts`).push(post);
+    await (() => {
+        dispatch(addPost({ id: ref.key, ...post }));
+        currentPost = ref.key;
+    })();
 
-                // Listen for state changes, errors, and completion of the upload.
-                uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, function(snapshot) {
-                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-                    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                    console.log('Upload is ' + progress + '% done');
-                    switch (snapshot.state) {
-                        case firebase.storage.TaskState.PAUSED: // or 'paused'
-                        console.log('Upload is paused');
-                        break;
-                        case firebase.storage.TaskState.RUNNING: // or 'running'
-                        console.log('Upload is running');
-                        break;
-                    }
-                    }, function(error) {
-    
-                    }, function() {
-                        // Upload completed successfully, now we can get the download URL
-                        console.log('Upload complete.')
-                        downloadURL = uploadTask.snapshot.downloadURL;
-                        return database.ref(`posts/${currentPost}`).update({ 
-                            postPicture: downloadURL,
-                            postPictureName: fileName
-                        })
-                    });
+    if (image) {
+        const fileName = image.name;
+        const storageRef = storage.ref(`blogPictures/${fileName}`);
+        const uploadTask = storageRef.put(image);
+        let downloadURL;
+
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, function(snapshot) {
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED: // or 'paused'
+                console.log('Upload is paused');
+                break;
+                case firebase.storage.TaskState.RUNNING: // or 'running'
+                console.log('Upload is running');
+                break;
             }
-        })
+            }, function(error) {
+
+            }, function() {
+                // Upload completed successfully, now we can get the download URL
+                console.log('Upload complete.')
+                downloadURL = uploadTask.snapshot.downloadURL;
+                return database.ref(`posts/${currentPost}`).update({ 
+                    postPicture: downloadURL,
+                    postPictureName: fileName
+                })
+            });
     }
 }
 
@@ -79,22 +75,19 @@ export const removePost = (id) => ({
     type: "REMOVE_POST",
     id
 });
-export const startRemovePost = (id, imageName) => {
-    return (dispatch) => {
-        return database.ref(`posts/${id}`).remove().then(() => {
-            dispatch(removePost(id));
-        }).then(() => {
-            if (imageName) {
-                const storageRef = storage.ref(`blogPictures/${imageName}`);
-                storageRef.delete().then(() => {
-                    console.log('Image deleted');
-                }).catch((error) => {
-                    console.log(error);
-                });
-            }
-        });
-    }
-}
+export const startRemovePost = (id, imageName) => async dispatch => {
+        await ( () => database.ref(`posts/${id}`).remove() )();
+        dispatch(removePost(id));
+
+        if (imageName) {
+            const storageRef = storage.ref(`blogPictures/${imageName}`);
+            storageRef.delete().then(() => {
+                console.log('Image deleted');
+            }).catch((error) => {
+                console.log(error);
+            });
+        }
+};
 
 
 // SET POSTS
@@ -102,22 +95,20 @@ export const setPosts = (posts) => ({
     type: "SET_POSTS",
     posts
 });
-export const startSetPosts = () => {
-    return (dispatch) => {
-        // For each post in the database, add the post to an array to be sent to the Redux store. 
-        return database.ref(`posts`).once('value').then((snapshot) => {
-            let posts = [];
+export const startSetPosts = () => async dispatch => {
+    // For each post in the database, add the post to an array to be sent to the Redux store. 
+    const snapshot = await database.ref(`posts`).once('value');
+    (() => {
+        let posts = [];
 
-            snapshot.forEach((childSnapshot) => {
-                posts.push({
-                    id: childSnapshot.key,
-                    ...childSnapshot.val()
-                });
+        snapshot.forEach((childSnapshot) => {
+            posts.push({
+                id: childSnapshot.key,
+                ...childSnapshot.val()
             });
-
-            dispatch(setPosts(posts));
         });
-    };
+        dispatch(setPosts(posts));
+    })();
 };
 
 
@@ -127,49 +118,42 @@ export const updatePost = (id, updates) => ({
     id,
     updates
 });
-export const startUpdatePost = (id, updates, currentPicture, picture) => {
-    return (dispatch) => {
-        return database.ref(`posts/${id}`).update({
-            title: updates.title,
-            body: updates.body
-        }).then(() => {
-            dispatch(updatePost(id, updates));
-        }).then(() => {
-            if (picture) {
-                const fileName = picture.name;
-                const storageRef = storage.ref(`/blogPictures/${fileName}`);
-                const uploadTask = storageRef.put(picture);
-                let downloadURL;
+export const startUpdatePost = (id, updates, currentPicture, picture) => async dispatch => {
+    await (() => database.ref(`posts/${id}`).update({ title: updates.title, body: updates.body }))();
+    dispatch(updatePost(id, updates));
 
-                // Listen for state changes, errors, and completion of the upload.
-                uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, function(snapshot) {
-                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-                var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                console.log('Upload is ' + progress + '% done');
-                switch (snapshot.state) {
-                    case firebase.storage.TaskState.PAUSED: // or 'paused'
-                    console.log('Upload is paused');
-                    break;
-                    case firebase.storage.TaskState.RUNNING: // or 'running'
-                    console.log('Upload is running');
-                    break;
-                }
-                }, function(error) {
+    if (picture) {
+        const fileName = picture.name;
+        const storageRef = storage.ref(`/blogPictures/${fileName}`);
+        const uploadTask = storageRef.put(picture);
+        let downloadURL;
 
-                }, function() {
-                    // Upload completed successfully, now we can get the download URL
-                    console.log('Upload complete.')
-                    downloadURL = uploadTask.snapshot.downloadURL;
-                    return database.ref(`posts/${id}`).update({ 
-                        postPicture: downloadURL,
-                        postPictureName: fileName
-                    }).then(() => {
-                        if (currentPicture && currentPicture !== fileName) {
-                            return storage.ref(`/blogPictures/${currentPicture}`).delete();
-                        }
-                    });
-                });
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, function(snapshot) {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+            case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+        }, function(error) {
+
+        }, async function() {
+            // Upload completed successfully, now we can get the download URL
+            console.log('Upload complete.')
+            downloadURL = uploadTask.snapshot.downloadURL;
+            await (() => database.ref(`posts/${id}`).update({ 
+                postPicture: downloadURL,
+                postPictureName: fileName
+            }))();
+            if (currentPicture && currentPicture !== fileName) {dispatch(removePost(id));
+                return storage.ref(`/blogPictures/${currentPicture}`).delete();
             }
-        })
+        });
     }
 }
