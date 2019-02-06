d032658ccfef35ec07e373374acb2614d33742a4
Rea Newell <patrick.r.newell@gmail.com>
2019-02-05 22:33:26 -0600

    changed focus outline to double purple

--- src/components/SummarizedPost.js
+++ src/components/SummarizedPost.js
@@ -3,12 +3,10 @@ import { Link } from 'react-router-dom';
 import dateFormatter from '../tools/dateFormatter';
 
 export const SummarizedPost = (props) => (
-    <Link to={`/posts/${props.post.title.replace(/ /g, "-")}`}>
-        <div className="summarized-post" >
+    <Link to={`/posts/${props.post.title.replace(/ /g, "-")}`} className="summarized-post">
             { props.post.postPicture && <img className="summarized-post__image" src={props.post.postPicture} alt={props.post.postPictureName}/>}
             <h2 className="summarized-post__title">{props.post.title}</h2>
             <p className="summarized-post__date">{`Posted ${dateFormatter(props.post.postDate.day, props.post.postDate.month, props.post.postDate.year)}`}</p>
-        </div>
     </Link>
 );
 
